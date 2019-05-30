06ff8592ee8d791824e35193dba309e43395e42b
Joey Scott <39806732+joeyscott-webdev@users.noreply.github.com>
2019-05-30 08:58:37 -0700

    Update SkillList.js
    Added fetch to personal website json data to add skills.

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class SkillList extends Component {
+  state = { skills: [] };
+
   componentWillMount() {
-    //fetch api here
+    fetch('https://joeywebdev.com/skills.json')
+    .then((response) => response.json())
+    .then((responseJson) => this.setState({ skills: responseJson }))
+    .catch((error) => {
+      console.error(error);
+    });
   }
 
 
   render() {
+    console.log(this.state);
+    
     return (
       <View>
         <Text>Skill List!!!</Text>
