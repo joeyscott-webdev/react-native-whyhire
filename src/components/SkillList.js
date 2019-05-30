1fed1399014e07ba22dd4c930ad2c7811df5d2ba
Joey Scott <39806732+joeyscott-webdev@users.noreply.github.com>
2019-05-30 06:23:58 -0700

    Update SkillList.js
    Removing axios dependancy in favor of native fetch api

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import axios from 'axios';
 
 class SkillList extends Component {
   componentWillMount() {
-    axios.get('https://joeywebdev.com/skills.json')
-      .then(response => {
-        this.setState({ skills: response.data });
-        //console.log(this.state.albums);
-        console.log(response);
-      })
-      .then(error => console.log(error));
+    //fetch api here
   }
 
 
